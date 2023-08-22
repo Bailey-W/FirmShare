4cec34d4a230c44a3c34fdf30c3ec966e4a94f04
NerosJourney <sandman4264@gmail.com>
2023-08-22 13:23:54 -0400

    Improved debug and documentation

--- linker/serial_connector.py
+++ linker/serial_connector.py
@@ -9,39 +9,55 @@ class SerialConnector:
         self.connected = False
 
     # Connects to the previously specified port at the given baud rate
+    # returns true if the connection is successful, or false if it is not
     def connect(self):
         if self.debug:
             print('Connecting to', self.port, 'at', self.baud, 'bps...')
-        self.ser = serial.Serial(self.port, self.baud, timeout=1)
-        if self.ser.name == self.port:
-            self.connected = True
-        if self.debug:
-            print("Connected to ", self.ser.name)
+        
+        # Tries to connect to the serial port
+        try:
+            # Defines the connection parameters for the serial connection
+            self.ser = serial.Serial(self.port, self.baud, timeout=1)
+            # Checks to make sure that the name of the connected port is as expected
+            if self.ser.name == self.port:
+                self.connected = True
+                if self.debug:
+                    print("Connected to ", self.ser.name)
+        except:
+            self.connected = False
+            if self.debug:
+                print("Failed to connect to serial port")
+
         return self.connected
     
+    # Disconnects from the serial port, only if previously connected
     def disconnect(self):
         if self.connected:
             if self.debug:
                 print("Disconnecting...")
             self.ser.close()
             self.connected = False
+        else:
+            if self.debug:
+                print("Unable to disconnect. Never connected.")
 
+
+    # Updates the port value of the object
+    # expects: port, a COM port of the computer
     def set_port(self, port):
         if self.connected:
             self.disconnect()
         self.port = port
 
+    # Updates the baud rate
+    # expects: baud, integer containing the new baud rate
     def set_baud(self, baud):
         if self.connected:
             self.disconnect()
         self.baud = baud
-    
-    
-
 
 if __name__ == "__main__":
     conn = SerialConnector('/dev/ttyUSB0', 115200, True)
-    conn.connect()
-    sleep(5)
+    print(conn.connect())
     conn.disconnect()
     
